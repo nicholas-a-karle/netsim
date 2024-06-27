5a953fc1d643c96c83d28a8e7dc7bc88fe1499c3
Elrak01 <105533430+nicholas-a-karle@users.noreply.github.com>
2024-06-27 10:26:08 -0500

    testing version

--- raw_social_graph.py
+++ raw_social_graph.py
@@ -9,10 +9,11 @@ class SocialGraph:
     def add_node(self, node):
         self.graph.add_node(node)
 
-    def add_edge(self, node1, node2, weight = True):
-        if weight not in (True, False):
-            raise ValueError("Edge weight must be True or False")
-        self.graph.add_edge(node1, node2, weight=weight)
+    def add_edge(self, node1, node2):
+        self.graph.add_edge(node1, node2)
+
+    def remove_edge(self, node1, node2):
+        self.graph.remove_edge(node1, node2)
 
     def has_node(self, node):
         return self.graph.has_node(node)
@@ -20,10 +21,8 @@ class SocialGraph:
     def has_edge(self, node1, node2):
         return self.graph.has_edge(node1, node2)
 
-    def get_edge_weight(self, node1, node2):
-        if self.graph.has_edge(node1, node2):
-            return self.graph[node1][node2]['weight']
-        return None
+    def get_edge(self, node1, node2):
+        return self.graph.has_edge(node1, node2)
 
     def get_neighbors(self, node):
         return list(self.graph.successors(node))
@@ -51,10 +50,23 @@ class SocialGraph:
             clusters[label].append(node)
         
         return list(clusters.values())
+    
+    def display_simple(self):
+        print(f"Raw Social Graph:")
+        connectivity = self.graph.number_of_edges() / ((self.graph.number_of_nodes() - 1) * self.graph.number_of_nodes())
+        print(f"\tNumber of nodes: {self.graph.number_of_nodes()}")
+        print(f"\tNumber of edges: {self.graph.number_of_edges()}")
+        print(f"\tConnectivity:    {connectivity:.4f}")
+        return self.graph.number_of_nodes(), self.graph.number_of_edges(), connectivity
 
-    def display(self):
+    def display_complex(self):
+        print(f"Raw Social Graph:")
         for node in self.graph.nodes:
             print(f"{node}: {self.get_neighbors(node)}")
         print("Edges with weights:")
         for edge in self.graph.edges(data=True):
             print(f"{edge[0]} -> {edge[1]}: {edge[2]['weight']}")
+
+    def display(self, complex = False):
+        if complex: return self.display_complex()
+        else: return self.display_simple()
--- social_graph.py
+++ social_graph.py
@@ -43,64 +43,178 @@ def to_circle_from_social(socialGraph: SocialGraph, n=2, m=5, self_enumerate=Tru
 
     return circleGraph
 
-#
-# runs a basic simulation on a raw Social Graph
-# type indicates what level of complexity it does
-# where r = reconnectivity
-# where P = P(edge appears)
-# type:
-# random    -> Complete Random (base case)          P = r / (max(|E|) - |E|)
-# neighbors -> Neighbors increase likelihood        P = r * |N| / (avg(|N|) * (max(|E|) - |E|))
-# distance  -> Distance decreases likelihood        P = r * avg(|D|) / (D * (max(|E|) - |E|))
-#                   avg(|D|) is graph wide for all pairs, not for single node
-# 
-# This function maintains a similar amount of reconnections no matter the type or state of the graph
-# If a different reconnecitivty is needed, use the functin social_sim_reconnect (tbd)
-# 
-def reconnect(socialGraph: SocialGraph, type = "distance", reconnectivity = 0.1) -> None:
-
-    max_e_min_e = socialGraph.graph.number_of_nodes * (socialGraph.graph.number_of_nodes - 1) - socialGraph.graph.number_of_edges
-    
-    # random    -> Complete Random (base case)          P = r / (max(|E|) - |E|)
+def reconnect(socialGraph: SocialGraph, type = "distance", reconnectivity = 0.1, base_rand = 0.2) -> None:
+    #
+    # runs a basic simulation on a raw Social Graph
+    # type indicates what level of complexity it does
+    # where r = reconnectivity
+    # where P = P(edge appears)
+    # type:
+    # random    -> Complete Random (base case)          P = r
+    # neighbors -> Neighbors increase likelihood        P = r * |N| / avg(|N|)
+    # distance  -> Distance decreases likelihood        P = r * avg(D) / D
+    # distrand  -> Distance decreases likelihood        P = (r * avg(D) / D) * a + (r) * b
+    #                   avg(|D|) is graph wide for all pairs, not for single node
+    #                   a and b set in formula
+    # 
+    # This function maintains a similar amount of reconnections no matter the type or state of the graph
+    # If a different reconnecitivty is needed, use the functin social_sim_reconnect (tbd)
+    # 
+    # base_rand: takes proportion of function away and makes it only based on the reconnectivity
+    # 
+    # random    -> Complete Random (base case)          P = r 
     if type == "random":
         # check each node
-        for i in range(socialGraph.graph.number_of_nodes):
-            for j in range(socialGraph.graph.number_of_nodes):
+        for i in range(socialGraph.graph.number_of_nodes()):
+            for j in range(socialGraph.graph.number_of_nodes()):
                 if i == j or socialGraph.has_edge(i, j): continue
-                p = reconnectivity / max_e_min_e
+                p = reconnectivity
+                #print(f"Prob({i}, \t{j}): \t{p}")
                 if random.random() < p: socialGraph.add_edge(i, j)
     
-    # neighbors -> Neighbors increase likelihood        P = r * |N| / (avg(|N|) * (max(|E|) - |E|))
+    # neighbors -> Neighbors increase likelihood        P = r * |N| / avg(|N|)
     elif type == "neighbors":
 
         # find avg(|N|)
         avg_n = sum(dict(socialGraph.graph.degree()).values()) / len(socialGraph.graph.nodes())
 
         # check each node
-        for i in range(socialGraph.graph.number_of_nodes):
-            for j in range(socialGraph.graph.number_of_nodes):
+        for i in range(socialGraph.graph.number_of_nodes()):
+            for j in range(socialGraph.graph.number_of_nodes()):
                 if i == j or socialGraph.has_edge(i, j): continue
-                p = reconnectivity * len(socialGraph.get_neighbors(i)) / (avg_n * max_e_min_e)
+                p = reconnectivity * len(socialGraph.get_neighbors(i)) / avg_n
+                p = (1 - base_rand) * p + base_rand * reconnectivity
+                #print(f"Prob({i}, \t{j}): \t{p}")
                 if random.random() < p: socialGraph.add_edge(i, j)
     
-    # distance  -> Distance decreases likelihood        P = r * avg(|D|) / (D * (max(|E|) - |E|))
+    # distance  -> Distance decreases likelihood        P = r * avg(D) / D
     elif type == "distance":
 
         # find distances
-        dist = nx.floyd_warshall(socialGraph)
+        dist = nx.floyd_warshall(socialGraph.graph)
 
-        # get the average distance
-        total_dist = 0
-        count = 0
-        for source, target_lengths in dist.items():
-            for target, length in target_lengths.items():
-                total_dist += length
-                count += 1
-        avg_dist = total_dist / count
+        # get the average distances
+        avg_dist = [0.0 for i in range(len(socialGraph.graph))]
+        for i in range(len(socialGraph.graph)):
+            count = 0
+            for j in range(len(socialGraph.graph)):
+                if dist[i][j] != float("inf"):
+                    count += 1
+                    avg_dist[i] += dist[i][j]
+            if count != 0:
+                avg_dist[i] /= count
+            else:
+                avg_dist[i] = float("inf")
 
         # check each node
-        for i in range(socialGraph.graph.number_of_nodes):
-            for j in range(socialGraph.graph.number_of_nodes):
+        for i in range(socialGraph.graph.number_of_nodes()):
+            for j in range(socialGraph.graph.number_of_nodes()):
                 if i == j or socialGraph.has_edge(i, j): continue
-                p = reconnectivity * avg_dist / (dist[i][j] * max_e_min_e)
+
+                if avg_dist[i] == float("inf") or dist[i][j] == float("inf"):
+                    p = 0
+                else:
+                    p = reconnectivity * avg_dist[i] / dist[i][j]
+
+                p = (1 - base_rand) * p + base_rand * reconnectivity
+                #print(f"Prob({i}, \t{j}): \t{p}")
                 if random.random() < p: socialGraph.add_edge(i, j)
+
+def deconnect(socialGraph: SocialGraph, type = "distance", deconnectivity = 0.1, base_rand = 0.2) -> None:
+
+    # fundamentally the same formula as above, but runs deconnections
+    #
+    # runs a basic simulation on a raw Social Graph
+    # type indicates what level of complexity it does
+    # where r = deconnectivity
+    # where P = P(edge disappears)
+    # type:
+    # random    -> Complete Random (base case)          P = r
+    # neighbors -> Neighbors decrease likelihood        P = r * avg(|N|) / |N|
+    # distance  -> Distance increases likelihood        P = r * D / avg(D)
+    #                   avg(|D|) is graph wide for all pairs, not for single node
+    # 
+    # This function maintains a similar amount of reconnections no matter the type or state of the graph
+    # If a different reconnecitivty is needed, use the functin social_sim_reconnect (tbd)
+    # 
+    # random    -> Complete Random (base case)          P = r 
+    if type == "random":
+        # check each node
+        for i in range(socialGraph.graph.number_of_nodes()):
+            for j in range(socialGraph.graph.number_of_nodes()):
+                if i == j or not socialGraph.has_edge(i, j): continue
+                p = deconnectivity
+                if random.random() < p: socialGraph.remove_edge(i, j)
+    
+    # neighbors -> Neighbors increase likelihood        P = r * |N| / avg(|N|)
+    elif type == "neighbors":
+
+        # find avg(|N|)
+        avg_n = sum(dict(socialGraph.graph.degree()).values()) / len(socialGraph.graph.nodes())
+
+        # check each node
+        for i in range(socialGraph.graph.number_of_nodes()):
+            for j in range(socialGraph.graph.number_of_nodes()):
+                if i == j or not socialGraph.has_edge(i, j): continue
+                p = deconnectivity * avg_n / len(socialGraph.get_neighbors(i))
+                p = (1 - base_rand) * p + base_rand * deconnectivity
+                if random.random() < p: socialGraph.remove_edge(i, j)
+                
+    
+    # distance  -> Distance decreases likelihood        P = r * avg(D) / D
+    elif type == "distance":
+
+        # find distances
+        dist = nx.floyd_warshall(socialGraph.graph)
+
+        # get the average distances
+        avg_dist = [0.0 for i in range(len(socialGraph.graph))]
+        for i in range(len(socialGraph.graph)):
+            count = 0
+            for j in range(len(socialGraph.graph)):
+                if dist[i][j] != float("inf"):
+                    count += 1
+                    avg_dist[i] += dist[i][j]
+            if count != 0:
+                avg_dist[i] /= count
+            else:
+                avg_dist[i] = float("inf")
+
+        # check each node
+        for i in range(socialGraph.graph.number_of_nodes()):
+            for j in range(socialGraph.graph.number_of_nodes()):
+                if i == j or not socialGraph.has_edge(i, j): continue
+
+                if avg_dist[i] == float("inf") or dist[i][j] == float("inf"):
+                    # Impossible for disconnect, left here anyways
+                    p = 1
+                else:
+                    p = deconnectivity * dist[i][j] / avg_dist[i]
+                
+                p = (1 - base_rand) * p + base_rand * deconnectivity
+                if random.random() < p: socialGraph.remove_edge(i, j)
+
+def soc_sim(socialGraph: SocialGraph, dunbar = 100):
+    # function to semi-realistically simulate socialization
+    # all members of the graph have no geography
+    # this means that the physical distance is not considered
+    # only social distance will affect them
+    
+    # there will be a dunbar's number established
+    # eventually will add ranks of dunbar-ity
+    # ex:
+    # rank          max
+    # known         ~ 500
+    # acquanited    ~ 200
+    # friend        ~ 100
+    # close         ~ 50
+    # best          ~ 10
+    #
+    # dunbar's number will establish a multiplier to interactions
+    # 
+    # each person will
+    # 
+
+
+
+    return
